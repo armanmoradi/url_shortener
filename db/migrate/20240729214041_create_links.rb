f7cf397f46c2c2b62655edf2b091b798688243dd
armanmoradi <armanmoradi8@gmail.com>
2024-08-13 00:12:50 +0330

    add chartkick and groupdate gem

--- db/migrate/20240729214041_create_links.rb
+++ db/migrate/20240729214041_create_links.rb
@@ -4,7 +4,7 @@ class CreateLinks < ActiveRecord::Migration[7.0]
       t.string :url
       t.string :description
       t.string :image
-      t.integer :views_count
+      t.integer :views_count, default: 0
 
       t.timestamps
     end
